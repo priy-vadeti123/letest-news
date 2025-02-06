6958a8996ec22c746aadf2a605fbe0ed22b92c69
priy-vadeti123 <priyavadeti2007@gmail.com>
2025-02-06 16:29:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+    learnMoreBtn.addEventListener("click", function () {
+        alert("Early detection includes self-exams, mammograms, and regular medical check-ups. Consult your doctor for more information.");
+    });
+});
+``
